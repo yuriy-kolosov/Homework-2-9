2fdfbafc4fca69ab06084488ae494b88be00754c
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2024-03-29 00:29:08 +0300

    Домашнее задание 2-9. Stream API и Optional - начальный коммит

---
+++ src/main/java/pro/sky/homework29/JavaSkyProHomework29Application.java
@@ -0,0 +1,13 @@
+package pro.sky.homework29;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSkyProHomework29Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSkyProHomework29Application.class, args);
+	}
+
+}
